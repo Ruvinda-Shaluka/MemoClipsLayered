f59e439f2ad3090332ccc9de11f9ff7123952bac
Ruvinda24 <shalukaofficial24@gmail.com>
2025-08-12 20:35:40 +0530

    factory completed

--- src/main/java/lk/ijse/memoclipsinlayered/bo/BOFactory.java
+++ src/main/java/lk/ijse/memoclipsinlayered/bo/BOFactory.java
@@ -1,4 +1,69 @@
 package lk.ijse.memoclipsinlayered.bo;
 
+
 public class BOFactory {
+    private static BOFactory boFactory;
+    private BOFactory() {}
+
+    public static BOFactory getInstance() {
+        return (boFactory == null) ? boFactory = new BOFactory() : boFactory;
+    }
+
+    public enum BOTypes {
+        ADMIN, ALBUM_DETAIL, ASSISTANT, BOOKING, CUSTOMER, INVOICE, ITEM,
+        PAYMENT, PHOTO_ALBUM, PHOTOGRAPHER, PHOTO_SESSION, PHOTO_STORAGE,
+        SUPPLIER, VIDEOGRAPHER, VIDEO_SESSION
+    }
+
+    public SuperBO getSuperBO(BOTypes boType) {
+        switch (boType) {
+            case ADMIN:
+                //return new AdminBOImpl();
+            case ALBUM_DETAIL:
+                // return new AlbumDetailBOImpl();
+                break;
+            case ASSISTANT:
+                // return new AssistantBOImpl();
+                break;
+            case BOOKING:
+                // return new BookingBOImpl();
+                break;
+            case CUSTOMER:
+                // return new CustomerBOImpl();
+                break;
+            case INVOICE:
+                // return new InvoiceBOImpl();
+                break;
+            case ITEM:
+                // return new ItemBOImpl();
+                break;
+            case PAYMENT:
+                // return new PaymentBOImpl();
+                break;
+            case PHOTO_ALBUM:
+                // return new PhotoAlbumBOImpl();
+                break;
+            case PHOTOGRAPHER:
+                // return new PhotographerBOImpl();
+                break;
+            case PHOTO_SESSION:
+                // return new PhotoSessionBOImpl();
+                break;
+            case PHOTO_STORAGE:
+                // return new PhotoStorageBOImpl();
+                break;
+            case SUPPLIER:
+                // return new SupplierBOImpl();
+                break;
+            case VIDEOGRAPHER:
+                // return new VideographerBOImpl();
+                break;
+            case VIDEO_SESSION:
+                // return new VideoSessionBOImpl();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid BO type");
+        }
+        throw new UnsupportedOperationException("BO implementation not found for: " + boType);
+    }
 }
--- src/main/java/lk/ijse/memoclipsinlayered/dao/DAOFactory.java
+++ src/main/java/lk/ijse/memoclipsinlayered/dao/DAOFactory.java
@@ -1,4 +1,68 @@
 package lk.ijse.memoclipsinlayered.dao;
 
+import lk.ijse.memoclipsinlayered.dao.custom.impl.AdminDAOImpl;
+
 public class DAOFactory {
+    private static DAOFactory daoFactory;
+    private DAOFactory() {}
+    public static DAOFactory getInstance() {
+        return (daoFactory == null) ? daoFactory = new DAOFactory() : daoFactory;
+    }
+
+    public enum DAOTypes{
+        ADMIN,ALBUM_DETAIL,ASSISTANT,BOOKING,CUSTOMER,INVOICE,ITEM,PAYMENT,
+        PHOTO_ALBUM,PHOTOGRAPHER,PHOTO_SESSION,PHOTO_STORAGE,SUPPLIER,VIDEOGRAPHER,VIDEO_SESSION;
+    }
+
+    public SuperDAO getSuperDAO(DAOTypes daoType) {
+        switch (daoType) {
+            case ADMIN:
+                //return new AdminDAOImpl();
+            case ALBUM_DETAIL:
+                // return new AlbumDetailDAOImpl();
+                break;
+            case ASSISTANT:
+                // return new AssistantDAOImpl();
+                break;
+            case BOOKING:
+                // return new BookingDAOImpl();
+                break;
+            case CUSTOMER:
+                // return new CustomerDAOImpl();
+                break;
+            case INVOICE:
+                // return new InvoiceDAOImpl();
+                break;
+            case ITEM:
+                // return new ItemDAOImpl();
+                break;
+            case PAYMENT:
+                // return new PaymentDAOImpl();
+                break;
+            case PHOTO_ALBUM:
+                // return new PhotoAlbumDAOImpl();
+                break;
+            case PHOTOGRAPHER:
+                // return new PhotographerDAOImpl();
+                break;
+            case PHOTO_SESSION:
+                // return new PhotoSessionDAOImpl();
+                break;
+            case PHOTO_STORAGE:
+                // return new PhotoStorageDAOImpl();
+                break;
+            case SUPPLIER:
+                // return new SupplierDAOImpl();
+                break;
+            case VIDEOGRAPHER:
+                // return new VideographerDAOImpl();
+                break;
+            case VIDEO_SESSION:
+                // return new VideoSessionDAOImpl();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid DAO type");
+        }
+        throw new UnsupportedOperationException("DAO implementation not found for: " + daoType);
+    }
 }
